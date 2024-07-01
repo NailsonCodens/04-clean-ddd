260e0c00c16603851b432a44f68b8bbc50ca3698
Nailson <nailson@192.168.1.2>
2024-07-01 07:21:41 -0300

    random id

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,9 +1,12 @@
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityId } from "../../core/entities/unique-entitiy-id";
 
 interface answerProps{
   content: string, 
-  authorId: string, 
-  questionId: string,   
+  authorId: UniqueEntityId, 
+  questionId: UniqueEntityId,  
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Answer extends Entity<answerProps>{
