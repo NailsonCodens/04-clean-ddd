8397b8f7f94f295283ee8a1cf856104306eed58a
Nailson <nailson@192.168.1.19>
2024-07-13 10:12:52 -0300

    add testes and delete question use case

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -2,8 +2,21 @@ import { QuestionsRepository } from "@/domain/forum/application/repositories/que
 import { Question } from "@/domain/forum/enterprise/entities/question";
 
 export class InMemoryQuestionsRepository implements QuestionsRepository{
+
+
     public items: Question[] = []
 
+    async findByid(id: string){
+        const question = this.items.find(item => item.id.toString() === id)
+
+        if(!question){
+            return null
+        }
+
+        return question
+
+    }
+    
     async findBySlug(slug: string) {
         const question = this.items.find(item => item.slug.value === slug)
 
@@ -17,4 +30,10 @@ export class InMemoryQuestionsRepository implements QuestionsRepository{
     async create(question: Question) {
         this.items.push(question)
     }
+
+    async delete(question: Question): Promise<void> {
+        const intemIndex = this.items.findIndex(item => item.id === question.id)
+
+        this.items.splice(intemIndex, 1)
+    }    
 }
